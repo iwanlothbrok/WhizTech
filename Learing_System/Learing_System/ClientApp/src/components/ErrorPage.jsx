0ab6bc44c961f3a553d464afd27e0d964cd1b12e
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-25 09:40:08 +0300

    Custom error component was created

---
+++ Learing_System/Learing_System/ClientApp/src/components/ErrorPage.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <div className="container-fluid bg-primary py-5 mb-5 page-header">
+        <div className="container py-5">
+          <div className="row justify-content-center">
+            <div className="col-lg-10 text-center">
+              <h1 className="display-3 text-white animated slideInDown">Not Found</h1>
+              <nav aria-label="breadcrumb">
+                <ol className="breadcrumb justify-content-center">
+                  <li className="breadcrumb-item">
+                    <Link className="text-white" to="/">Home</Link>
+                  </li>
+                  <li className="breadcrumb-item">
+                    <Link className="text-white" to="#">Pages</Link>
+                  </li>
+                  <li className="breadcrumb-item text-white active" aria-current="page">404</li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* 404 Start */}
+      <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div className="container text-center">
+          <div className="row justify-content-center">
+            <div className="col-lg-6">
+              <i className="bi bi-exclamation-triangle display-1 text-primary"></i>
+              <h1 className="display-1">404</h1>
+              <h1 className="mb-4">Page Not Found</h1>
+              <p className="mb-4">We’re sorry, the page you have looked for does not exist on our website! Maybe go to our home page or try to use a search?</p>
+              <Link className="btn btn-primary rounded-pill py-3 px-5" to="/">Go Back To Home</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
