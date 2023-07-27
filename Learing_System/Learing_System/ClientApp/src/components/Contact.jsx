ee127529edc55bcd7af4b96eda20a07b02022187
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-07-27 11:02:46 +0300

    Contact was changed and now is something similar to register

--- Learing_System/Learing_System/ClientApp/src/components/Contact.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import '../styles/contact.css'
 const Contact = () => {
   const [status, setStatus] = useState('');
 
@@ -10,107 +10,147 @@ const Contact = () => {
     // Clear the form fields after submission (optional)
     e.target.reset();
   };
-
   return (
-    <section className="h-100 bg-dark">
+    <section className="h-100 h-custom gradient-custom-2">
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col">
-            <div className="card card-registration my-4">
-              <div className="row g-0">
-                <div className="col-xl-6 d-none d-xl-block">
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/img4.webp"
-                    alt="Sample photo"
-                    className="img-fluid"
-                    style={{ borderTopLeftRadius: '.25rem', borderBottomLeftRadius: '.25rem' }}
-                  />
-                </div>
-                <div className="col-xl-6">
-                  <div className="card-body p-md-5 text-black">
-                    <h3 className="mb-5 text-uppercase">Student registration form</h3>
+          <div className="col-12">
+            <div className="card card-registration card-registration-2" style={{ borderRadius: '15px' }}>
+              <div className="card-body p-0">
+                <div className="row g-0">
+                  <div className="col-lg-6">
+                    <div className="p-5">
+                      <h3 className="fw-normal mb-5" style={{ color: '#4835d4' }}>General Information</h3>
 
-                    <div className="row">
-                      <div className="col-md-6 mb-4">
+                      <div className="mb-4 pb-2">
+                        <select className="select">
+                          <option value="1">Title</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                          <option value="4">Four</option>
+                        </select>
+                      </div>
+
+                      <div className="row">
+                        <div className="col-md-6 mb-4 pb-2">
+                          <div className="form-outline">
+                            <input type="text" id="form3Examplev2" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form3Examplev2">First name</label>
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4 pb-2">
+                          <div className="form-outline">
+                            <input type="text" id="form3Examplev3" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form3Examplev3">Last name</label>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className="mb-4 pb-2">
+                        <select className="select">
+                          <option value="1">Position</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                          <option value="4">Four</option>
+                        </select>
+                      </div>
+
+                      <div className="mb-4 pb-2">
                         <div className="form-outline">
-                          <input type="text" id="form3Example1m" className="form-control form-control-lg" />
-                          <label className="form-label" htmlFor="form3Example1m">Вашите три имена:</label>
+                          <input type="text" id="form3Examplev4" className="form-control form-control-lg" />
+                          <label className="form-label" htmlFor="form3Examplev4">Position</label>
+                        </div>
+                      </div>
+
+                      <div className="row">
+                        <div className="col-md-6 mb-4 pb-2 mb-md-0 pb-md-0">
+                          <div className="form-outline">
+                            <input type="text" id="form3Examplev5" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form3Examplev5">Business Arena</label>
+                          </div>
+                        </div>
+                        <div className="col-md-6">
+                          <select className="select">
+                            <option value="1">Employees</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                            <option value="4">Four</option>
+                          </select>
                         </div>
                       </div>
                     </div>
+                  </div>
 
-                    <div className="row">
-                      <div className="col-md-6 mb-4">
-                        <div className="form-outline">
-                          <input type="text" id="form3Example1m1" className="form-control form-control-lg" />
-                          <label className="form-label" htmlFor="form3Example1m1">Три имена на родител:</label>
+                  <div className="col-lg-6 bg-indigo text-white">
+                    <div className="p-5">
+                      <h3 className="fw-normal mb-5">Contact Details</h3>
+
+                      <div className="mb-4 pb-2">
+                        <div className="form-outline form-white">
+                          <input type="text" id="form3Examplea2" className="form-control form-control-lg" />
+                          <label className="form-label" htmlFor="form3Examplea2">Street + Nr</label>
                         </div>
                       </div>
-                    </div>
 
-                    <div className="row">
-                      <div className="col-md-6 mb-4">
-                        <div className="form-outline">
-                          <input type="text" id="form3Example1m1" className="form-control form-control-lg" />
-                          <label className="form-label" htmlFor="form3Example1m1">Телефон за връзка:</label>
+                      <div className="mb-4 pb-2">
+                        <div className="form-outline form-white">
+                          <input type="text" id="form3Examplea3" className="form-control form-control-lg" />
+                          <label className="form-label" htmlFor="form3Examplea3">Additional Information</label>
                         </div>
                       </div>
-                    </div>
 
-                    <div className="form-outline mb-4">
-                      <label className="form-label" htmlFor="form3Example8">Адрес:</label>
-                      <input type="text" id="form3Example8" className="form-control form-control-lg" />
-                    </div>
-
-                    <div className="d-md-flex justify-content-start align-items-center mb-4 py-2">
-                      <h6 className="mb-0 me-4">Студент ли сте: </h6>
-
-                      <div className="form-check form-check-inline mb-0 me-4">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="inlineRadioOptions"
-                          id="femaleGender"
-                          value="option1"
-                        />
-                        <label className="form-check-label" htmlFor="femaleGender">Да</label>
+                      <div className="row">
+                        <div className="col-md-5 mb-4 pb-2">
+                          <div className="form-outline form-white">
+                            <input type="text" id="form3Examplea4" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form3Examplea4">Zip Code</label>
+                          </div>
+                        </div>
+                        <div className="col-md-7 mb-4 pb-2">
+                          <div className="form-outline form-white">
+                            <input type="text" id="form3Examplea5" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form3Examplea5">Place</label>
+                          </div>
+                        </div>
                       </div>
 
-                      <div className="form-check form-check-inline mb-0 me-4">
-                        <input
-                          className="form-check-input"
-                          type="radio"
-                          name="inlineRadioOptions"
-                          id="maleGender"
-                          value="option2"
-                        />
-                        <label className="form-check-label" htmlFor="maleGender">Не</label>
+                      <div className="mb-4 pb-2">
+                        <div className="form-outline form-white">
+                          <input type="text" id="form3Examplea6" className="form-control form-control-lg" />
+                          <label className="form-label" htmlFor="form3Examplea6">Country</label>
+                        </div>
                       </div>
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form3Example90">Град:</label>
-                        <input type="text" id="form3Example90" className="form-control form-control-lg" />
+                      <div className="row">
+                        <div className="col-md-5 mb-4 pb-2">
+                          <div className="form-outline form-white">
+                            <input type="text" id="form3Examplea7" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form3Examplea7">Code +</label>
+                          </div>
+                        </div>
+                        <div className="col-md-7 mb-4 pb-2">
+                          <div className="form-outline form-white">
+                            <input type="text" id="form3Examplea8" className="form-control form-control-lg" />
+                            <label className="form-label" htmlFor="form3Examplea8">Phone Number</label>
+                          </div>
+                        </div>
                       </div>
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form3Example90">Пощенски код:</label>
-                        <input type="text" id="form3Example90" className="form-control form-control-lg" />
+                      <div className="mb-4">
+                        <div className="form-outline form-white">
+                          <input type="text" id="form3Examplea9" className="form-control form-control-lg" />
+                          <label className="form-label" htmlFor="form3Examplea9">Your Email</label>
+                        </div>
                       </div>
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form3Example99">Курс:</label>
-                        <input type="text" id="form3Example99" className="form-control form-control-lg" />
+                      <div className="form-check d-flex justify-content-start mb-4 pb-3">
+                        <input className="form-check-input me-3" type="checkbox" value="" id="form2Example3c" />
+                        <label className="form-check-label text-white" htmlFor="form2Example3">
+                          I do accept the <a href="#!" className="text-white"><u>Terms and Conditions</u></a> of your site.
+                        </label>
                       </div>
 
-                      <div className="form-outline mb-4">
-                        <label className="form-label" htmlFor="form3Example97">Имейл:</label>
-                        <input type="text" id="form3Example97" className="form-control form-control-lg" />
-                      </div>
-
-                      <div className="d-flex justify-content-end pt-3">
-                        <button type="button" className="btn btn-light btn-lg">Reset all</button>
-                        <button type="button" className="btn btn-warning btn-lg ms-2">Submit form</button>
-                      </div>
+                      <button type="button" className="btn btn-light btn-lg" data-mdb-ripple-color="dark">Register</button>
                     </div>
                   </div>
                 </div>
@@ -119,7 +159,6 @@ const Contact = () => {
           </div>
         </div>
       </div>
-
     </section>
   );
 };
