e21eee5861a3369d16124e61e710eb107f940d7f
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-08-16 13:30:38 +0300

    singe exercises component was added to show every specific language exercises, exercises component was complete changed

---
+++ Learing_System/Learing_System/ClientApp/src/components/exercises/Exercises.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+// import backgroundImage from '../../styles/c#.jpg.avif'; // Adjust the path based on your file structure
+import backgroundImage from '../../styles/background2.jpg.avif';
+import SingleExercise from './SingleExercise';
+const Exercise = () => {
+
+    const sectionStyle = {
+        backgroundImage: `url(${backgroundImage})`, // Use the imported background image
+        backgroundSize: 'cover', // Adjust this according to your preference
+        backgroundPosition: 'center', // Adjust this according to your preference
+        width: '100vw',  // Make the section cover the full viewport width
+        height: '100vh'
+    };
+
+    const h1Style = {
+        color: 'white',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Add semi-transparent black background
+        padding: '20px',
+        borderRadius: '10px',
+    };
+
+    // const formStyle = {
+    //     backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust the opacity and color as needed
+    //     padding: '20px', // Add padding to the form to separate it from the background
+    //     borderRadius: '10px', // Add rounded corners for a better look
+    // };
+
+    const { language } = useParams();
+    const exercise = [{
+        id: '5',
+        title: 'Zadacha 1.',
+        description: 'imame 5 qbylki i izqjdame 3, kolko imame',
+        answers: ['2', '4', '1', '3']
+    },
+    {
+        id: '7',
+        title: 'Zadacha 1.',
+        description: 'imame 5 qbylki i izqjdame 3, kolko imame',
+        answers: ['2', '4', '1', '3']
+    },
+    {
+        id: '6',
+        title: 'Zadacha 1.',
+        description: 'imame 5 qbylki i izqjdame 3, kolko imame',
+        answers: ['2', '4', '1', '3']
+    }]
+
+
+    return (
+        <div className="container mt-5" style={sectionStyle}>
+            <h1 className="text-center mb-4" style={h1Style}>C# Exercises</h1>
+            <SingleExercise title={language} exercises={exercise} />
+        </div>
+    );
+};
+
+export default Exercise;
---
+++ Learing_System/Learing_System/ClientApp/src/components/exercises/SingleExercise.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+
+const SingleExercise = ({ language, exercises }) => {
+
+   
+
+    return (
+        <div className="accordion mt-3" id="accordionPanelsStayOpenExample">
+            <h1>{language}</h1>
+            {exercises.map((exercise, index) => (
+                <div className="accordion-item mt-2 mb-2" key={exercise.id}>
+                    <h2 className="accordion-header" id={`heading-${exercise.id}`}>
+                        <button
+                            className={`accordion-button ${index === 0 ? '' : 'collapsed'}`}
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target={`#exercise-collapse-${exercise.id}`}
+                            aria-expanded={index === 0 ? 'true' : 'false'}
+                            aria-controls={`exercise-collapse-${exercise.id}`}
+                        >
+                            {exercise.title}
+                        </button>
+                    </h2>
+                    <div
+                        id={`exercise-collapse-${exercise.id}`}
+                        className={`accordion-collapse collapse ${index === 0 ? 'show' : ''}`}
+                        aria-labelledby={`heading-${exercise.id}`}
+                        data-bs-parent="#accordionPanelsStayOpenExample"
+                    >
+                        <div className="accordion-body">
+                            <strong>{exercise.description}</strong>
+                            {exercise.answers.map((answer, answerIndex) => (
+                                <p key={answerIndex}>{answer}</p>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default SingleExercise;
