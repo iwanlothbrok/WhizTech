d5d1ef8504bcd24340d496c1818d49f1b4f211f6
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-08-17 17:53:13 +0300

    Exercises controller was created and some validations should be added + implementing services

---
+++ Learing_System/Learing_System/Controllers/ExerciseController.cs
@@ -0,0 +1,33 @@
+﻿using Learning_System.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace Learing_System.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ExerciseController : ControllerBase
+	{
+		private readonly ApplicationDbContext context;
+
+		public ExerciseController(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+
+		[HttpGet]
+		public Task Get(string lang)
+		{
+			//make service which get the id of this LANG
+			//then change the where to be PL.Id == gettedId
+
+			return context.Exercises
+					.Include(c => c.Answers)
+					// that should not work properly 
+					.Where(e => e.ProgrammingLanguage.Name == "C#")
+					.ToListAsync();
+		}
+	}
+}
