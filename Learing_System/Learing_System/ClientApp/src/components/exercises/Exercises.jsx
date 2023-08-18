33eaa5abb4565e859e5a6a752c67ccb8101fbf58
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-08-18 17:57:09 +0300

    exercises component is working properly with fetching the data from the api

--- Learing_System/Learing_System/ClientApp/src/components/exercises/Exercises.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/exercises/Exercises.jsx
@@ -1,52 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-
+import axios from 'axios';
 import backgroundImage from '../../styles/background2.jpg.avif';
 import SingleExercise from './SingleExercise';
+
 const Exercise = () => {
     const { lang } = useParams();
+    const [isLoading, setIsLoading] = useState(false);
+    const [exercise, setExercises] = useState([]);
 
     const sectionStyle = {
-        backgroundImage: `url(${backgroundImage})`, // Use the imported background image
-        backgroundSize: 'cover', // Adjust this according to your preference
-        backgroundPosition: 'center', // Adjust this according to your preference
-        width: '100vw',  // Make the section cover the full viewport width
+        backgroundImage: exercise.length === 0 ? 'none' : `url(${backgroundImage})`,
+        backgroundPosition: 'center',
+        backgroundSize: exercise.length === 0 ? 'auto' : 'cover', // Adjust the background size based on the condition
+        width: '100vw',
         height: '100vh'
     };
 
     const h1Style = {
         color: 'white',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Add semi-transparent black background
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
         padding: '20px',
         borderRadius: '10px',
     };
 
-    const exercise = [{
-        id: '5',
-        title: 'Zadacha 1.',
-        description: 'imame 5 qbylki i izqjdame 3, kolko imame',
-        answers: ['2', '4', '1', '3']
-    },
-    {
-        id: '7',
-        title: 'Zadacha 1.',
-        description: 'imame 5 qbylki i izqjdame 3, kolko imame',
-        answers: ['2', '4', '1', '3']
-    },
-    {
-        id: '6',
-        title: 'Zadacha 1.',
-        description: 'imame 5 qbylki i izqjdame 3, kolko imame',
-        answers: ['2', '4', '1', '3']
-    }]
+    const fetchExercisesFromAPI = async () => {
+        try {
+            setIsLoading(true);
 
+            const response = await axios.get('https://localhost:7089/api/exercise/', {
+                headers: {
+                    lang: lang
+                }
+            });
 
+            setExercises(response.data);
+        } catch (error) {
+            console.error('Error fetching exercises:', error);
+        }
+    };
 
-    console.log(exercise);
+    useEffect(() => {
+        fetchExercisesFromAPI();
+    }, [lang]);
+
     return (
         <div className="container mt-5" style={sectionStyle}>
             <h1 className="text-center mb-4" style={h1Style}>{lang}</h1>
-            <SingleExercise exercises={exercise} />
+            {exercise.length === 0 ? (
+                <div className="text-center mt-5" style={{ fontSize: '48px' }}>
+                    There are no exercises for this language.
+                </div>) : (
+                <SingleExercise exercises={exercise} />
+            )}
         </div>
     );
 };
