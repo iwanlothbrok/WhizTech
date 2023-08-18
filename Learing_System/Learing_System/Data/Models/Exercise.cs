b836316a2a4f5851a0c9b8e24f95ffde1a372b42
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-08-18 17:55:12 +0300

    database changes on the tables

---
+++ Learing_System/Learing_System/Data/Models/Exercise.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learning_System.Data.Models
+{
+    public class Exercise
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public int ProgrammingLanguageId { get; set; }
+        public ProgrammingLanguage? ProgrammingLanguage { get; set; }
+
+        public ICollection<Answer> Answers { get; set; } = new List<Answer>();
+    }
+}
