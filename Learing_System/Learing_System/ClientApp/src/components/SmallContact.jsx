227bb75ed26b83c3212a10668269311f4eab6470
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-08-29 23:13:11 +0300

    our info section was added to small contact form

--- Learing_System/Learing_System/ClientApp/src/components/SmallContact.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/SmallContact.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-
+import ContactInfoSection from './contact/ContactInfoSection'
 export default function SmallContact() {
 
     const [formData, setFormData] = useState({
@@ -48,72 +48,127 @@ export default function SmallContact() {
         });
     }
     return (
-        <div className="container-fluid  justify-content-center align-items-center" style={{ backgroundColor: '#232323' }}>
-            <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h3 className="mb-5 text-white">Contact us</h3>
-            </div>
-            {successMessageVisible && (
-                <div className="alert alert-success" role="alert">
-                    Form submitted successfully!
-                </div>
-            )}
-            <form className="mx-md-5">
-                <div className="row">
-                    <div className="col-md-3 mb-1 pb-2">
-                        <input
-                            type="text"
-                            className="form-control half-width"
-                            id="input-name"
-                            placeholder="Name"
-                            value={formData.FullName}
-                            onChange={(e) => setFormData({ ...formData, FullName: e.target.value })}
-                        />
+        //             <div className="row justify-content-center">
+        <div style={{ backgroundColor: '#232323' }}>
+
+            <div className="container">
+
+                <div className="row container-fluid  justify-content-center align-items-center" style={{ backgroundColor: '#232323' }}>
+                    <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
+                        <h3 className="mb-5 text-white">Contact us</h3>
                     </div>
-                    <div className="col-md-4 mb-1 pb-3">
-                        <input
-                            type="email"
-                            className="form-control half-width"
-                            id="input-email"
-                            placeholder="Email address"
-                            value={formData.Email}
-                            onChange={(e) => setFormData({ ...formData, Email: e.target.value })}
-                        />
+                    {successMessageVisible && (
+                        <div className="alert alert-success" role="alert">
+                            Form submitted successfully!
+                        </div>
+                    )}
+
+                    <div class="col">
+                        <form className="mx-md-5">
+                            <div className="row">
+                                <div className="col-md-5 mb-1 pb-2">
+                                    <input
+                                        type="text"
+                                        className="form-control half-width"
+                                        id="input-name"
+                                        placeholder="Name"
+                                        value={formData.FullName}
+                                        onChange={(e) => setFormData({ ...formData, FullName: e.target.value })}
+                                    />
+                                </div>
+                                <div className="col-md-7 mb-1 pb-3">
+                                    <input
+                                        type="email"
+                                        className="form-control half-width"
+                                        id="input-email"
+                                        placeholder="Email address"
+                                        value={formData.Email}
+                                        onChange={(e) => setFormData({ ...formData, Email: e.target.value })}
+                                    />
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-13 mb-4 pb-2">
+                                    <input
+                                        type="text"
+                                        className="form-control "
+                                        id="input-subject"
+                                        placeholder="Phone"
+                                        value={formData.PhoneNumber}
+                                        onChange={(e) => setFormData({ ...formData, PhoneNumber: e.target.value })}
+                                    />
+                                </div>
+                                <div className="col-md-13 mb-4 pb-2">
+                                    <textarea
+                                        className="form-control half-width"
+                                        id="input-message"
+                                        placeholder="Message"
+                                        rows="7"
+                                        value={formData.Description}
+                                        onChange={(e) => setFormData({ ...formData, Description: e.target.value })}
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <div className="text-center">
+                                <button
+                                    type="submit"
+                                    className="btn btn-outline-secondary text-white p-4 px-5 mt-2 mx-auto"
+                                    id="input-submit"
+                                    onClick={handleSubmit}
+                                    style={{ borderRadius: '20px' }}
+                                >
+                                    Submit
+                                </button>
+                            </div>
+
+                        </form>
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-7 mb-4 pb-2">
-                        <input
-                            type="text"
-                            className="form-control half-width"
-                            id="input-subject"
-                            placeholder="Phone"
-                            value={formData.PhoneNumber}
-                            onChange={(e) => setFormData({ ...formData, PhoneNumber: e.target.value })}
-                        />
+
+                    {/* information */}
+                    <div class="col-6" style={{ backgroundColor: '#71F483' }}>
+                        {/* <div className="col-md-5 d-flex align-items-stretch"> */}
+                        <div className="info-wrap w-100 p-lg-5 p-4">
+                            <h3 className="mb-4 mt-md-4">Contact us</h3>
+                            <div className="dbox w-100 d-flex align-items-start">
+                                <div className="icon d-flex align-items-center justify-content-center">
+                                    <span className="fa fa-map-marker"></span>
+                                </div>
+                                <div className="text pl-3">
+                                    <p>
+                                        <span>Address:</span> 198 West 21th Street, Suite 721 New York NY
+                                        10016
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="dbox w-100 d-flex align-items-start">
+                                <div className="icon d-flex align-items-center justify-content-center">
+                                    <span className="fa fa-map-marker"></span>
+                                </div>
+                                <div className="text pl-3">
+                                    <p>
+                                        <span>Phone:</span> 198 West 21th Street, Suite 721 New York NY
+                                        10016
+                                    </p>
+                                </div>
+                            </div> <div className="dbox w-100 d-flex align-items-start">
+                                <div className="icon d-flex align-items-center justify-content-center">
+                                    <span className="fa fa-map-marker"></span>
+                                </div>
+                                <div className="text pl-3">
+                                    <p>
+                                        <span>Email:</span> 198 West 21th Street, Suite 721 New York NY
+                                        10016
+                                    </p>
+                                </div>
+                            </div>
+
+                            {/* </div> */}
+                        </div>
                     </div>
-                    <div className="col-md-7 mb-4 pb-2">
-                        <textarea
-                            className="form-control half-width"
-                            id="input-message"
-                            placeholder="Message"
-                            rows="5"
-                            value={formData.Description}
-                            onChange={(e) => setFormData({ ...formData, Description: e.target.value })}
-                        ></textarea>
-                    </div>
+
                 </div>
-                <div className="text-center">
-                    <button
-                        type="submit"
-                        className="btn btn-outline-secondary text-white p-4 px-5 mt-2 mx-auto"
-                        id="input-submit"
-                        onClick={handleSubmit}
-                        style={{ borderRadius: '20px' }}
-                    >
-                        Submit
-                    </button>
-                </div>
-            </form>
+            </div >
         </div>
+
     );
 }
