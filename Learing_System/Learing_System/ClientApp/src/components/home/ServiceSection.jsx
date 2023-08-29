f0143c6e8c3c930ae2b151705f08d7b75346b0f9
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-08-29 23:13:23 +0300

    service section background was changed

--- Learing_System/Learing_System/ClientApp/src/components/home/ServiceSection.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/home/ServiceSection.jsx
@@ -2,42 +2,45 @@ import React from 'react';
 
 function ServiceSection() {
   return (
-    <div className="container-xxl py-5">
-      <div className="container">
-        <div className="row g-4">
-          <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-            <div className="service-item text-center pt-3">
-              <div className="p-4">
-                <i className="fa fa-3x fa-graduation-cap text-alert mb-4"></i>
-                <h5 className="mb-3">Студенти</h5>
-                <p>имащи нужда от помощ с материала изучаван в университета, подготовка за изпити или курсови работи</p>
+    <div style={{ backgroundColor: '#232323' }}>
+
+      <div className="container-xxl py-5">
+        <div className="container">
+          <div className="row g-4">
+            <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+              <div className="service-item text-center pt-3">
+                <div className="p-4">
+                  <i className="fa fa-3x fa-graduation-cap text-alert mb-4"></i>
+                  <h5 className="mb-3">Студенти</h5>
+                  <p>имащи нужда от помощ с материала изучаван в университета, подготовка за изпити или курсови работи</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-            <div className="service-item text-center pt-3">
-              <div className="p-4">
-                <i className="fa fa-3x fa-globe text-alert mb-4"></i>
-                <h5 className="mb-3">Самообучаващи</h5>
-                <p>амбициозни и целеустремини, искайки да научат важните концпеции на програмирането самостоятелно</p>
+            <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+              <div className="service-item text-center pt-3">
+                <div className="p-4">
+                  <i className="fa fa-3x fa-globe text-alert mb-4"></i>
+                  <h5 className="mb-3">Самообучаващи</h5>
+                  <p>амбициозни и целеустремини, искайки да научат важните концпеции на програмирането самостоятелно</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-            <div className="service-item text-center pt-3">
-              <div className="p-4">
-                <i className="fa fa-3x fa-home text-alert mb-4"></i>
-                <h5 className="mb-3">Ученици</h5>
-                <p>които имат нужда от помощ с учебния материал, или искат да се развиват в сферата на програмирането и технологиите</p>
+            <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
+              <div className="service-item text-center pt-3">
+                <div className="p-4">
+                  <i className="fa fa-3x fa-home text-alert mb-4"></i>
+                  <h5 className="mb-3">Ученици</h5>
+                  <p>които имат нужда от помощ с учебния материал, или искат да се развиват в сферата на програмирането и технологиите</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-            <div className="service-item text-center pt-3">
-              <div className="p-4">
-                <i className="fa fa-3x fa-book-open text-alert mb-4"></i>
-                <h5 className="mb-3">Online Videos and Resourses</h5>
-                <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+            <div className="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
+              <div className="service-item text-center pt-3">
+                <div className="p-4">
+                  <i className="fa fa-3x fa-book-open text-alert mb-4"></i>
+                  <h5 className="mb-3">Online Videos and Resourses</h5>
+                  <p>Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+                </div>
               </div>
             </div>
           </div>
