dde256fd5be37eba6db64deca292be1042191e98
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-08-30 14:45:49 +0300

    contact info sections is created and is working properly

--- Learing_System/Learing_System/ClientApp/src/components/contact/ContactInfoSection.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/contact/ContactInfoSection.jsx
@@ -2,31 +2,72 @@ import React from 'react';
 
 const ContactInfoSection = () => {
     return (
-        <div className="col-md-5 d-flex align-items-stretch">
-            <div className="info-wrap bg-primary w-100 p-lg-5 p-4">
-                <h3 className="mb-4 mt-md-4">Contact us</h3>
-                <div className="dbox w-100 d-flex align-items-start">
-                    <div className="icon d-flex align-items-center justify-content-center">
-                        <span className="fa fa-map-marker"></span>
+        <div class="col-6" style={{ backgroundColor: '#71F483' }}>
+            <div className="col-md-5 d-flex align-items-stretch">
+                <div className="info-wrap w-100 p-lg-5 p-4">
+                    <h3 className="mb-4 mt-md-4 display-4">Контакти</h3> {/* Use Bootstrap display-4 class for bigger text */}
+                    {/* address */}
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="d-flex flex-column flex-md-row align-items-center">
+                                <div class="icon d-flex align-items-center justify-content-center">
+                                    <span class="fa fa-map-marker fa-2x"></span> {/* Use fa-2x class for bigger icon */}
+                                </div>
+                                <div class="d-flex flex-column flex-md-row align-items-center">
+                                    <span className='p-2 p-md-4 h4'>Адрес: </span> {/* Use h4 class for bigger text */}
+                                    <p class="p-2 p-md-4 h4">Пловдив, България</p> {/* Use h4 class for bigger text */}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="text pl-3">
-                        <p>
-                            <span>Address:</span> 198 West 21th Street, Suite 721 New York NY
-                            10016
-                        </p>
 
+                    {/* phone */}
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="d-flex flex-column flex-md-row align-items-center">
+                                <div class="icon d-flex align-items-center justify-content-center">
+                                    <span class="fa fa-phone fa-2x"></span> {/* Use fa-2x class for bigger icon */}
+                                </div>
+                                <div class="d-flex flex-column flex-md-row align-items-center">
+                                    <span className='p-2 p-md-4 h4'>Телефон: </span> {/* Use h4 class for bigger text */}
+                                    <a href="tel:+08891013123" class="p-2 p-md-4 h4" style={{ fontSize: '1.5rem' }}>0889044614</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <span className="fa fa-map-marker"></span>
-                </div>
-                <div className="text pl-3">
-                    <p>
-                        <span>Address:</span> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Cum adipisci, tempore accusantium reiciendis dolorem culpa eaque laboriosam delectus corporis amet enim commodi, non, obcaecati optio illum laborum sint. Nesciunt, nemo.
-                    </p>
+                    {/* email */}
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="d-flex flex-column flex-md-row align-items-center">
+                                <div class="icon d-flex align-items-center justify-content-center">
+                                    <span class="fa fa-paper-plane fa-2x"></span> {/* Use fa-2x class for bigger icon */}
+                                </div>
+                                <div class="d-flex flex-column flex-md-row align-items-center">
+                                    <span className='p-2 p-md-4 h4'>Email: </span> {/* Use h4 class for bigger text */}
+                                    <a href="mailto:info@yoursite.com" class="p-2 p-md-4 h5">whiztech@gmail.com</a> {/* Use h4 class for bigger text */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* site */}
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="d-flex flex-column flex-md-row align-items-center">
+                                <div class="icon d-flex align-items-center justify-content-center">
+                                    <span class="fa fa-globe fa-2x"></span> {/* Use fa-2x class for bigger icon */}
+                                </div>
+                                <div class="d-flex flex-column flex-md-row align-items-center">
+                                    <span className='p-2 p-md-4 h4'>Website: </span> {/* Use h4 class for bigger text */}
+                                    <a href="#" class="p-2 p-md-4 h4">WhizTech.bg</a> {/* Use h4 class for bigger text */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </div>
-
             </div>
+
         </div >
     );
 };
