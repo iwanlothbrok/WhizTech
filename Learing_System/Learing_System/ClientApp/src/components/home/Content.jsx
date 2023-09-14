6c94b3eef982ee200d3fb7c0a9a19b2a5514556d
Iwan <iwaniwanow0345@gmail.com>
2023-09-14 11:14:32 +0300

    content text was changed

--- Learing_System/Learing_System/ClientApp/src/components/home/Content.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/home/Content.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import img from '../../styles/favicon.png'
 const Content = () => {
     return (
@@ -12,30 +13,27 @@ const Content = () => {
                             </div>
                         </div>
                         <div className="col-12 col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
-                            <h1 className="mb-4">Добре дошли в ЕУРОЦИ</h1>
-                            <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
-                            <p className="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                            <h1 className="mb-4">Добре дошли в WhizTech</h1>
+                            <p className="mb-4">Независимо дали сте начинаещ или опитен програмист, ние ви предлагаме персонализирани уроци, които ще ви помогнат да достигнете нови висоти в програмирането.</p>
+                            <p className="mb-4">В програмирането никога не спираме да учим и растем. Нашият образователен подход е създаден така, че да отговаря на вашите специфични нужди и ниво на опит. Ако сте начинаещ, нашите уроци ще ви предоставят твърда основа и ясно разбиране на основните концепции. Ако сте опитен програмист, нашите уроци ще ви помогнат да разширите вашите умения и да се издигнете на нови нива на експертност.</p>
                             <div className="row gy-2 gx-4 mb-4">
                                 <div className="col-12 col-sm-6">
-                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>Skilled Instructors</p>
+                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>Професионални Инструктори</p>
                                 </div>
                                 <div className="col-12 col-sm-6">
-                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>Online Classes</p>
+                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>Гъвкави Онлайн Уроци</p>
                                 </div>
                                 <div className="col-12 col-sm-6">
-                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>International Certificate</p>
+                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>Персонализиран Подход</p>
                                 </div>
                                 <div className="col-12 col-sm-6">
-                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>Skilled Instructors</p>
+                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>Онлайн Ресурси</p>
                                 </div>
-                                <div className="col-12 col-sm-6">
-                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>Online Classes</p>
-                                </div>
-                                <div className="col-12 col-sm-6">
-                                    <p className="mb-0"><i className="fa fa-arrow-right text-alert me-2"></i>International Certificate</p>
-                                </div>
+
                             </div>
-                            <a className="btn bg-black text-white py-3 px-5 mt-2" href="">Read More</a>
+                            <Link to='/contact'>
+                                <button  className="btn p-4 btn-lg  custom-button">Запиши се</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
