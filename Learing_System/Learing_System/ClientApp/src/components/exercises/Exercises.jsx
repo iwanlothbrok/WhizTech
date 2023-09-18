edd6b4d0d1501d0dd6a866a5a9aba0249d719611
Iwan <iwaniwanow0345@gmail.com>
2023-09-18 22:30:35 +0300

    exercises validations for lang, paging and getting only 3 were added

--- Learing_System/Learing_System/ClientApp/src/components/exercises/Exercises.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/exercises/Exercises.jsx
@@ -2,32 +2,100 @@ import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import SingleExercise from './SingleExercise';
 import csharpExercises from './exerDb/csharpExercises'
-
+import Pages from '../blog/Pages'
 const Exercise = () => {
     const { lang } = useParams();
     const [isLoading, setIsLoading] = useState(true); // Set initial loading state to true
     const [exercise, setExercises] = useState([csharpExercises]);
 
+    const [currentPage, setCurrentPage] = useState(1); // Track the current page
+    const exercisesPerPage = 6; // Number of exercises to show per page
+
+    const url = window.location.href;
+    const isStudyPage = url.includes('/study');
+
     useEffect(() => {
         setIsLoading(true);
-        if (lang === 'csharp') {
-            setTimeout(() => {
-                setIsLoading(false);
-                setExercises(csharpExercises);
-            }, 1000)
-        }
-        // else if (lang === 'sql') {
-        //     setExercises(csharpExercises);
-        // } else if (lang === 'javascript') {
-        //     setExercises(csharpExercises);
-        // } else if (lang === 'java') {
-        //     setExercises(csharpExercises);
-        // } else if (lang === 'cplus') {
-        //     setExercises(csharpExercises);
-        // }
-    }, [lang]);
 
-    console.log(lang);
+        if (isStudyPage) {
+            if (lang === 'csharp') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    setExercises(csharpExercises.slice(0, 3));
+                }, 1000)
+            }
+            else if (lang === 'javascript') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    // setExercises(csharpExercises.slice(0, 3));
+                }, 1000)
+            }
+            else if (lang === 'cplus') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    // setExercises(csharpExercises.slice(0, 3));
+                }, 1000)
+            }
+            else if (lang === 'sql') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    // setExercises(csharpExercises.slice(0, 3));
+                }, 1000)
+            }
+            else if (lang === 'java') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    // setExercises(csharpExercises.slice(0, 3));
+                }, 1000)
+            }
+        }
+        else {
+            if (lang === 'csharp') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    setExercises(csharpExercises);
+                }, 1000)
+            }
+            else if (lang === 'javascript') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    // setExercises(csharpExercises);
+                }, 1000)
+            }
+            else if (lang === 'cplus') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    // setExercises(csharpExercises);
+                }, 1000)
+            }
+            else if (lang === 'sql') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    // setExercises(csharpExercises);
+                }, 1000)
+            }
+            else if (lang === 'java') {
+                setTimeout(() => {
+                    setIsLoading(false);
+                    // setExercises(csharpExercises);
+                }, 1000)
+            }
+        }
+        const startIndex = (currentPage - 1) * exercisesPerPage;
+        const endIndex = startIndex + exercisesPerPage;
+        const slicedExercises = exercise.slice(startIndex, endIndex);
+        setExercises(slicedExercises);
+
+
+
+    }, [lang, currentPage]);
+
+
+
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    };
+
     console.log(exercise)
     return (
         <div className="container mt-5">
@@ -40,7 +108,16 @@ const Exercise = () => {
                     There are no exercises for this language.
                 </div>
             ) : (
-                <SingleExercise exercises={exercise} />
+                <div>
+
+                    <SingleExercise exercises={exercise} />
+                    <Pages
+                        currentPage={currentPage}
+                        totalPages={Math.ceil(csharpExercises.length / exercisesPerPage)} // Calculate total pages based on the total exercises
+                        onPageChange={handlePageChange}
+                    />
+                </div>
+
             )}
         </div>
     );
