02aa4c324fc3488c8862f30345851705afbea501
Iwan <iwaniwanow0345@gmail.com>
2023-09-19 12:08:05 +0300

    clients styles was changed

--- Learing_System/Learing_System/ClientApp/src/components/home/Clients.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/home/Clients.jsx
@@ -39,22 +39,23 @@ const Clients = () => {
         <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
             <div className="container">
                 <div className="owl-carousel testimonial-carousel position-relative" style={carouselStyle}  >
-                    <div className="testimonial-item text-center bg-black" >
+                    <div className="testimonial-item text-center bg-white">
                         <img className="border rounded-circle p-2 mx-auto mb-3" src={client} style={{ width: '80px', height: '80px' }} alt="Client 1" />
-                        <h5 className="mb-0 text-white ">Николай Желев</h5>
-                        <p className='text-white'>Студент по Софтуерни технологии</p>
-                        <div className="testimonial-text bg-black text-center p-4 text-white ">
-                            <p className="mb-0">Уроците промениха живота ми, обичам ги.</p>
-                        </div>
-                    </div>
-                    <div className="testimonial-item text-center bg-black">
-                        <img className="border rounded-circle p-2 mx-auto mb-3" src={client} style={{ width: '80px', height: '80px' }} alt="Client 1" />
-                        <h5 className="mb-0 text-white ">Client Name</h5>
-                        <p className='text-white'>Profession</p>
-                        <div className="testimonial-text bg-black text-center p-4 text-white ">
+                        <h5 className="mb-0 text-black ">Client Name</h5>
+                        <p className='text-black'>Profession</p>
+                        <div className="testimonial-text bg-white text-center p-4 text-black ">
                             <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
                         </div>
                     </div>
+                    <div className="testimonial-item text-center bg-white">
+                        <img className="border rounded-circle p-2 mx-auto mb-3" src={client} style={{ width: '80px', height: '80px' }} alt="Client 1" />
+                        <h5 className="mb-0 text-black ">Client Name</h5>
+                        <p className='text-black'>Profession</p>
+                        <div className="testimonial-text bg-white text-center p-4 text-black ">
+                            <p className="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
