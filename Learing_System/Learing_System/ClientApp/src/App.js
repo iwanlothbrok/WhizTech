44646f48ef2e475a3d6f075a64ac4d7ea91a9ac4
Iwan <iwaniwanow0345@gmail.com>
2023-09-25 18:08:00 +0300

    routing everything which is not valid to error page and languages information was cutted

--- Learing_System/Learing_System/ClientApp/src/App.js
+++ Learing_System/Learing_System/ClientApp/src/App.js
@@ -6,9 +6,7 @@ import './styles/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/home/Home';
 import About from './components/about/About'
-import Courses from './components/courses/Courses';
 import ErrorPage from './components/ErrorPage';
-import Contact from './components/Contact'
 import AddCourse from './components/courses/AddCourse';
 import Exercise from './components/exercises/Exercises';
 import Calendar from './components/calendar/Calendar';
@@ -24,12 +22,13 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<ContactWithInformation />} />
         <Route path="/courses/add" element={<AddCourse />} />
-        <Route path="/404" element={<ErrorPage />} />
+        <Route path="/error" element={<ErrorPage />} />
         <Route path="/exer/:lang" element={<Exercise />} />
         <Route path="/study/:lang" element={<Language />} />
         <Route path="/cal" element={<Calendar />} />
         <Route path="/blog" element={<BlogList />} />
         <Route path="/blog/:blogId" element={<SelectedBlog />} />
+        <Route path="/*" element={<ErrorPage />} />
 
       </Routes>
     </div>
--- Learing_System/Learing_System/ClientApp/src/components/languages/langDb/javascript.js
+++ Learing_System/Learing_System/ClientApp/src/components/languages/langDb/javascript.js
@@ -1,11 +1,9 @@
 import img from '../../../styles/js-logo.png';
 const reasonsToChooseJavaScript = {
     versatility: "JavaScript е универсален език, използван за уеб разработка, програмиране на сървърната страна и разработка на мобилни приложения.",
-    browserIntegration: "JavaScript се изпълнява нативно в уеб браузърите, позволявайки динамични и интерактивни уеб приложения.",
     richFrameworks: "Има богат набор от библиотеки и рамки като React, Angular и Vue.js, които опростяват уеб разработката.",
     modernLanguage: "Съвременните функции на JavaScript (ES6+) подобряват четливостта на кода и поддръжката му.",
     jobOpportunities: "Високото търсене на разработчици на JavaScript предлага множество възможности за кариера.",
-    communitySupport: "Жива онлайн общност и ресурси за документация улесняват ученето и решаването на проблеми.",
     integration: "Интегрирайте JavaScript с бази данни, API и други технологии.",
 };
 
@@ -14,11 +12,9 @@ const JavaScript = {
     langName: 'Java Script',
     information: [
         reasonsToChooseJavaScript.versatility,
-        reasonsToChooseJavaScript.browserIntegration,
         reasonsToChooseJavaScript.richFrameworks,
         reasonsToChooseJavaScript.modernLanguage,
         reasonsToChooseJavaScript.jobOpportunities,
-        reasonsToChooseJavaScript.communitySupport,
         reasonsToChooseJavaScript.integration,
     ],
     photoStyle: {
