7574d6ca9970e234a08cde31474ab49da732545f
Iwan <iwaniwanow0345@gmail.com>
2024-04-26 23:47:29 +0300

    Revert "testing logs"
    This reverts commit df899703c98258a81373f05c0cb425a51a76bb7c.

--- Learing_System/Learing_System/ClientApp/src/firebaseConfig.js
+++ Learing_System/Learing_System/ClientApp/src/firebaseConfig.js
@@ -12,8 +12,6 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-
-console.log(firebaseConfig);
 // Initialize Firestore
 const firebaseApp = initializeApp(firebaseConfig);
 
