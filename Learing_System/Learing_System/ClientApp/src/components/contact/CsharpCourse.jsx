6788939ad21af0f505c60671a3038259aff43227
Iwan <iwaniwanow0345@gmail.com>
2024-06-27 10:51:43 +0300

    classes in the csharp registration course were changed - also css was exported on different file

--- Learing_System/Learing_System/ClientApp/src/components/contact/CsharpCourse.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/contact/CsharpCourse.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import '../../styles/css/joinCourse.css';
+import '../../styles/css/csharpCourse.css';
 import backgroundImage from '../../styles/photos/courseBg.jpg';
 import { csharpCourseSubmitHandle } from '../../firebaseConfig';
 
@@ -105,72 +105,71 @@ export default function CsharpCourseForm() {
     };
 
     return (
-        <section style={sectionStyle}>
-            <div className="container d-flex justify-content-center align-items-center h-100">
-                <div className="form-wrapper">
+        <section style={sectionStyle} className="custom-section">
+            <div className="custom-container custom-d-flex custom-justify-content-center custom-align-items-center custom-h-100">
+                <div className="custom-form-wrapper">
                     {successMessageVisible && Object.keys(errors).length === 0 && (
-                        <div className="alert alert-success mb-0" role="alert">
+                        <div className="custom-alert custom-alert-success" role="alert">
                             Успешно изпратено запитване!
                         </div>
                     )}
                     {Object.keys(errors).length > 0 && submitButtonClicked && (
-                        <div className="alert alert-danger mb-0" role="alert">
+                        <div className="custom-alert custom-alert-danger" role="alert">
                             {Object.values(errors).map((error, index) => (
                                 <div key={index}>{error}</div>
                             ))}
                         </div>
                     )}
-                    <form className='formCourse mt-1' onSubmit={handleSubmit}>
-                        <h2 className='joinOurCourse text-center'><strong style={{ color: '#15b115' }}>ЗАПИШИ СЕ</strong></h2>
-                        <label className='labelCourse'><strong>ТРИ ИМЕНА:</strong></label>
+                    <form className='custom-formCourse' onSubmit={handleSubmit}>
+                        <h1 className='custom-joinOurCourse custom-text-center mt-2'><strong style={{ color: '#15b115', fontWeight: 'bold' }}>ЗАПИШИ СЕ</strong></h1>
+                        <label className='custom-labelCourse'><strong>ТРИ ИМЕНА:</strong></label>
                         <input
                             id="input-name"
-                            className='inputCourse'
+                            className='custom-inputCourse'
                             placeholder="Иван Иванов"
                             value={formData.FullName}
                             onChange={(e) => setFormData({ ...formData, FullName: e.target.value })}
                             required
                         />
 
-                        <label className='labelCourse' htmlFor="email"><strong>ИМЕЙЛ:</strong></label>
+                        <label className='custom-labelCourse' htmlFor="email"><strong>ИМЕЙЛ:</strong></label>
                         <input
                             id="email"
-                            className='inputCourse'
+                            className='custom-inputCourse'
                             placeholder="example@example.com"
                             value={formData.Email}
                             onChange={(e) => setFormData({ ...formData, Email: e.target.value })}
                             required
                         />
-                        <label className='labelCourse' htmlFor="phone"><strong>ТЕЛ. НОМЕР:</strong></label>
+                        <label className='custom-labelCourse' htmlFor="phone"><strong>ТЕЛ. НОМЕР:</strong></label>
                         <input
                             id="phone"
-                            className='inputCourse'
+                            className='custom-inputCourse'
                             placeholder="+359 88 888 8888"
                             value={formData.Phone}
                             onChange={(e) => setFormData({ ...formData, Phone: e.target.value })}
                             required
                         />
 
-
-
-                        <label className='labelCourse'>
+                        <label className='custom-labelCourse'>
                             <strong>ИМАТЕ ЛИ ПРЕДИШЕН ОПИТ?</strong>
                         </label>
-                        <div style={{ position: 'relative', height: '2rem', width: '8rem' }}>
+                        <div className="custom-checkbox-wrapper">
                             <input
                                 type="checkbox"
                                 id="toggle"
                                 checked={formData.PreviousExperienceCheck}
                                 onChange={(e) => setFormData({ ...formData, PreviousExperienceCheck: e.target.checked })}
+                                className="custom-checkbox"
                             />
                         </div>
 
                         {formData.PreviousExperienceCheck && (
                             <>
-                                <label className='labelCourse' htmlFor="previousExperience"><strong>КАКЪВ Е ВАШИЯ ОПИТ:</strong></label>
+                                <label className='custom-labelCourse' htmlFor="previousExperience"><strong>КАКЪВ Е ВАШИЯ ОПИТ:</strong></label>
                                 <textarea
                                     id="previousExperience"
-                                    className='inputCourse'
+                                    className='custom-inputCourse'
                                     placeholder="Опишете предишния си опит - с кой език сте се занимавали, до къде сте стигнали"
                                     value={formData.PreviousExperience}
                                     onChange={(e) => setFormData({ ...formData, PreviousExperience: e.target.value })}
@@ -178,10 +177,10 @@ export default function CsharpCourseForm() {
                             </>
                         )}
 
-                        <label className='labelCourse' htmlFor="leadSource"><strong>КАК НАУЧИХТЕ ЗА НАС?</strong></label>
+                        <label className='custom-labelCourse' htmlFor="leadSource"><strong>КАК НАУЧИХТЕ ЗА НАС?</strong></label>
                         <select
                             id="leadSource"
-                            className='inputCourse'
+                            className='custom-inputCourse'
                             value={formData.LeadSource}
                             onChange={(e) => setFormData({ ...formData, LeadSource: e.target.value })}
                         >
@@ -191,16 +190,16 @@ export default function CsharpCourseForm() {
                             <option value="advertisement">Реклама</option>
                         </select>
                         <button
-                            className='navBtn btn'
+                            className='custom-navBtn custom-btn'
                             type='submit'
                         >
                             <b>ИЗПРАТИ</b>
                         </button>
 
-                        <div className='iconsDiv p-1 text-center'>
-                            <span className="fa fa-phone icon"></span>+359 88 904 4614
+                        <div className='custom-iconsDiv custom-p-1 custom-text-center'>
+                            <span className="fa fa-phone custom-icon"></span>+359 88 904 4614
                             <br />
-                            <span className="fa fa-envelope-o icon"></span> whiztech.team@gmail.com
+                            <span className="fa fa-envelope-o custom-icon"></span> whiztech.team@gmail.com
                         </div>
                     </form>
                 </div>
