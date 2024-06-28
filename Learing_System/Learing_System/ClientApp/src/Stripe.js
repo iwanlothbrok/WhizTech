cc7db214bfeb9e0c1172caf3cfc77146c378ce9d
Iwan <iwaniwanow0345@gmail.com>
2024-06-28 16:45:18 +0300

    stripe was removed

--- Learing_System/Learing_System/ClientApp/src/Stripe.js
+++
@@ -1,5 +0,0 @@
-// src/Stripe.js
-import { loadStripe } from '@stripe/stripe-js';
-
-const stripePromise = loadStripe('STRIPE PUBLISHABLE KEY'); // Replace with your Stripe publishable key
-export default stripePromise;
