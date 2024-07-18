7742e2c4345a60a4172105428d155657946d4c70
Iwan <iwaniwanow0345@gmail.com>
2024-07-18 17:42:20 +0300

    Clients were improved

--- Learing_System/Learing_System/ClientApp/src/components/home/Client.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/home/Client.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
 function Client({ client }) {
-
     return (
-
-        <div className="testimonial-item text-center bg-secondary border p-3">
-            <img className="border rounded-circle p-1 mx-auto mb-3"
+        <div className="testimonial-item text-center bg-secondary border p-4" style={{ borderRadius: '10px' }}>
+            <img className="border rounded-circle p-2 mx-auto mb-3"
                 src={client.img}
-                style={{ width: '120px', height: '120px' }}
+                style={{ width: '120px', height: '120px', borderColor: '#71F483' }}
                 alt={client.fullName}
             />
-            <div className='row d-flex justify-content-center'>
-                <h5 className="mb-0 text-white ">{client.fullName}</h5>
-                <p className='text-white'>{client.profession}</p>
-                <div className="testimonial-text text-center p-2 text-white ">
-                    <p className="mb-0 ">{client.feedback}</p>
+            <div className="row d-flex justify-content-center">
+                <h4 className="mb-0 ">{client.fullName}</h4>
+                <p className="text-light">{client.profession}</p>
+                <div className="testimonial-text text-center p-3 text-light bg-dark rounded" style={{ borderColor: '#71F483' }}>
+                    <p className="mb-0">{client.feedback}</p>
                 </div>
             </div>
         </div>
--- Learing_System/Learing_System/ClientApp/src/components/home/Clients.jsx
+++ Learing_System/Learing_System/ClientApp/src/components/home/Clients.jsx
@@ -1,27 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import zd from '../../styles/photos/zdrawko.jpg'
-import clientPhoto2 from '../../styles/photos/vankata1.jpg'
-import clientPhoto3 from '../../styles/photos/yanislav.png'
+import React, { useEffect } from 'react';
+import zd from '../../styles/photos/zdrawko.jpg';
+import clientPhoto2 from '../../styles/photos/vankata1.jpg';
+import clientPhoto3 from '../../styles/photos/yanislav.png';
 import client04 from '../../styles/photos/clientLiubcho.jpg';
 import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel'; // Import Owl Carousel
+import 'owl.carousel';
 import 'animate.css/animate.min.css';
 import Client from './Client';
 import fadeInAnimation from '../../animation/fadeInAnimation';
 
 const Clients = () => {
     useEffect(() => {
-        // Define the elements you want to animate
         const elementsToAnimate = document.querySelectorAll('.hidden');
-
-        // Call the fadeInAnimation function with the elements
         fadeInAnimation(elementsToAnimate);
-    }, []); // 
+    }, []);
+
     const clients = [
         {
             img: client04,
             fullName: 'Любомир Радунтчев',
-            profession: 'Стажант в "Re4Life" и студент по "Информационни технологии" ',
+            profession: 'Стажант в "Re4Life" и студент по "Информационни технологии"',
             feedback: 'Уроците по C# и JavaScript на WhizTech бяха невероятни. Получих комплексно обучение, което ми позволи да изградя солидни умения в програмирането.'
         },
         {
@@ -29,7 +27,8 @@ const Clients = () => {
             fullName: 'Иван Малинков',
             profession: 'Студент по "Софтуерни Технологии и Дизайн"',
             feedback: 'Проучвах различни опции за обучение по C# и най-накрая избрах WhizTech. Беше най-доброто решение, което можех да взема. Научих се не само как да пиша код на C#, но и как да го правя по ефективен начин.'
-        }, {
+        },
+        {
             img: clientPhoto3,
             fullName: 'Янислав Димитров',
             profession: 'Ученик в Езикова Гимназия',
@@ -40,13 +39,15 @@ const Clients = () => {
             fullName: 'Здравко Караджов',
             profession: 'Студент по "Електротехника"',
             feedback: 'След уроците се справих успешно с изпитите по C++, получавайки добри оценки и придобивайки ценни умения в програмирането. Бих препоръчал уроците на всеки!'
-        },
-    ]
+        }
+    ];
 
     const carouselStyle = {
-        boxShadow: '5px 5px 10px rgba(255, 0, 0, 0.5);', /* Change the color here */
-        background: 'rgb(108, 117, 125)'
-    }
+        background: '#22CA7C', // Darker background for better contrast
+        borderRadius: '10px', // Rounded corners for a softer look
+        padding: '20px'
+    };
+
     useEffect(() => {
         if (window.$) {
             window.$('.testimonial-carousel').owlCarousel({
@@ -72,12 +73,11 @@ const Clients = () => {
         }
     }, []);
 
-
     return (
         <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
             <div className="container hidden">
                 <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h2 className="mb-2 text-white">Доволни клиенти</h2>
+                    <h2 className="mb-5 text-white">ДОВОЛНИ КЛИЕНТИ</h2>
                 </div>
                 <div className="owl-carousel testimonial-carousel position-relative" style={carouselStyle}>
                     {clients.map((testimonial, index) => (
@@ -90,6 +90,6 @@ const Clients = () => {
             </div>
         </div>
     );
-}
-export default Clients;
+};
 
+export default Clients;
